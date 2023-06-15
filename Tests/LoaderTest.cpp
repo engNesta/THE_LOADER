dcaff4674dc6336c8c40b55b5f18ab50c7c12082
engNesta <robert.nuhu0002@stud.hkr.se>
2023-06-15 08:57:38 +0200

    Testing suite created

---
+++ Tests/LoaderTest.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Nesta on 2023-06-15.
+//
+#define CATCH_CONFIG_MAIN
+#include "Catch2/src/catch2/catch_test_macros.hpp"
+#include "../Source/MainComponent.h"
+#include "../Source/MainComponent.cpp"
+#include <juce_core/juce_core.h>
+#include <juce_graphics/juce_graphics.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+
+TEST_CASE()
+{
+
+
+
+}
+
+TEST_CASE()
+{
+
+
+
+}
